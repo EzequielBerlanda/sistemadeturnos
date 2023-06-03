639e81fed365fcdcff0d108bd7ef6ca0a3f2a8ad
Ezequiel Berlanda <berlandaezequiel@gmail.com>
2023-06-03 10:22:57 -0300

    commit

--- main.js
+++ main.js
@@ -110,9 +110,43 @@ btnGuardar.addEventListener("click", function () {
   console.log(localStorage);
 
 
-ObjetoPacientes = JSON.parse(localStorage.getItem(`pacientes`))
+ObjetoPacientes = JSON.parse(localStorage.getItem(`pacientes`));
 console.log(ObjetoPacientes);
 
+});
+
+
+
+
+
+// MENU SELECT MP
+const dropdowns = document.querySelectorAll(".dropdown");
+
+// lopp through all drop down elements
+dropdowns.forEach((dropdown) => {
+  const select = dropdown.querySelector(".select");
+  const caret = dropdown.querySelector(".caret");
+  const menu = dropdown.querySelector(".menu");
+  const options = dropdown.querySelectorAll(".menu li");
+  const selected = dropdown.querySelector(".selected");
+  select.addEventListener("click", () => {
+    select.classList.toggle("select-clicked");
+    caret.classList.toggle("caret-rotate");
+    menu.classList.toggle("menu-open");
+  });
+
+  options.forEach((option) => {
+    option.addEventListener("click", () => {
+      selected.innerText = option.innerText;
+      select.classList.remove("select-clicked");
+      caret.classList.remove("caret-rotate");
+      menu.classList.remove("menu-open");
+      options.forEach((option) => {
+        option.classList.remove("active");
+      });
+      option.classList.add("active");
+    });
+  });
 });
 
 
